44d47a7e6cc577e21650e4d35112602c48a4387f
DeathMeat96 <dinca.marius96@gmail.com>
2024-03-31 13:02:41 +0300

    test 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,13 +10,13 @@ import { Link} from 'react-router-dom'
  
 const Header = () => {
   return <header className='bg-rose-200	fixed w-full px-[30px] lg:px-[100px] z-30 
-  h-[100px] lg:h-[148px] flex items-center'>
+  h-[50px] lg:h-[125px] flex items-center'>
     <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
 
-      <Link to={'/'} className='max-w-[150px] h-[100px] lg:max-w-[350px] lg:h-[180px]'>
+      <Link to={'/'} className='max-w-[100px] h-[40px] lg:max-w-[230px] lg:h-[90px]'>
         <img src={Logo} alt='' />
       </Link>
-      <nav className='hidden xl:flex gap-x-12 font-bold text-2xl'>
+      <nav className='hidden xl:flex gap-x-12 font-bold text-lg'>
         <Link
           to={'/'}
           className='text-[#696c6d] hover:text-primary transition'
