105aad70e4968114fbf475180c1a8de1eed580a5
DeathMeat96 <dinca.marius96@gmail.com>
2024-03-31 13:02:20 +0300

    Test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,17 +17,17 @@ const Home = () => {
   return <section className='section bg-white'>
     <div className='container mx-auto h-full relative'>
       <div className='flex flex-col justify-center'>
-        <div className='w-full pt-60 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:right-3/4 lg:absolute flex flex-col justify-center items-center lg:items-start gap-8 sm:gap-2'>
-          <p1 className='h1'>
+        <div className='w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:right-3/4 lg:absolute flex flex-col justify-center items-center lg:items-start gap-8 sm:gap-2 lg:gap-6'>
+          <p1 className='h1 sm:pt-0'>
           Magia baloanelor aduce zâmbete și emoții! </p1><br/> <p1 className='lg:text-2xl'> Lasă-ne să transformăm spațiul evenimentului tău  într-un loc memorabil.
           </p1> 
-          <Link to={'/contact'} className='btn mb-[30px]'>
+          <Link to={'/contact'} className='btn sm:mb-[2px] sm:text-sm'>
           Contact
           </Link>
         </div>
-          <div className='flex justify-end max-h-200 lg:max-h-max xl:max-h-max '>
-            <div className='relative lg:left-1/4 overflow-hidden sm:pt-1 pt-36 '>
-            <Carousel className='max-w-[1680px] lg:pt-36'>
+          <div className='flex lg:justify-end sm:justify-center max-h-200 lg:max-h-max xl:max-h-max '>
+            <div className='relative overflow-hidden sm:pt-0   pt-36  '>
+            <Carousel className='lg:max-w-[1100px] sm:max-w-[350px]  lg:pt-28'>
               <img src={Image1} alt=''/>
               <img src={Image3} alt='' />
               <img src={Image4} alt=''/>
